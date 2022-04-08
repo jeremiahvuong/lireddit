37f26dd6efaf552aac427b6ae3fa2f5c9337d47d
Jeremiah Vuong <43306105+jeremiahvuong@users.noreply.github.com>
2022-04-07 20:05:25 -0700

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/post";
+import mikroConfig from "./mikro-orm.config";
+
+const main = async () => {
+  const orm = await MikroORM.init(mikroConfig);
+
+  await orm.getMigrator().up();
+
+  // const post = orm.em.fork({}).create(Post, {
+  //   title: "my first post",
+  //   createdAt: new Date(),
+  //   updatedAt: new Date(),
+  // });
+  // await orm.em.persistAndFlush(post);
+
+  // const posts = await orm.em.find(Post, {});
+  // console.log(posts);
+};
+
+main().catch((err) => {
+  console.log(err);
+});
