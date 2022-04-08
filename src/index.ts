bd00a977e2d47e450ebf9d57de898bd6809badfa
Jeremiah Vuong <43306105+jeremiahvuong@users.noreply.github.com>
2022-04-08 11:51:10 -0700

    initial graphql Posts query

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,36 @@
+import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/post";
 import mikroConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig);
 
+  // refreshes migrations
   await orm.getMigrator().up();
 
-  // const post = orm.em.fork({}).create(Post, {
-  //   title: "my first post",
-  //   createdAt: new Date(),
-  //   updatedAt: new Date(),
-  // });
-  // await orm.em.persistAndFlush(post);
+  const app = express();
 
-  // const posts = await orm.em.find(Post, {});
-  // console.log(posts);
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em }),
+  });
+
+  await apolloServer.start(); // bandaid
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("Server started on localhost:4000");
+  });
 };
 
 main().catch((err) => {
