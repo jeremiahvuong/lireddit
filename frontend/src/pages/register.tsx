5d802631d174b5b0192a61ef330fbf168be441e0
Jeremiah Vuong <43306105+jeremiahvuong@users.noreply.github.com>
2022-04-10 01:27:43 -0700

    Feat: basic register submit form

---
+++ frontend/src/pages/register.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Formik, Form } from "formik";
+import { Box, Button } from "@chakra-ui/react";
+import { Wrapper } from "../components/Wrapper";
+import { InputField } from "../components/InputField";
+
+interface registerProps {}
+
+export const Register: React.FC<registerProps> = ({}) => {
+  return (
+    <Wrapper varient="small">
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField
+              name="username"
+              placeholder="username"
+              label="Username"
+            />
+            <Box mt={4}>
+              <InputField
+                name="passowrd"
+                placeholder="password"
+                label="Password"
+                type="password"
+              />
+            </Box>
+            <Button
+              type="submit"
+              mt={4}
+              isLoading={isSubmitting}
+              colorScheme="teal"
+            >
+              register
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
+};
+
+export default Register;
