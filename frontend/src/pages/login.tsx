eafbaf905027bd37efd3a82090d8017f37951776
Jeremiah Vuong <43306105+jeremiahvuong@users.noreply.github.com>
2022-04-14 17:33:09 -0700

    Feat: Next.js URQL SSR

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -6,6 +6,8 @@ import { InputField } from "../components/InputField";
 import { useLoginMutation } from "../generated/graphql";
 import { toErrorMap } from "../utils/toErrorMap";
 import { useRouter } from "next/router";
+import { withUrqlClient } from "next-urql";
+import { createUrqlClient } from "../utils/createUrqlClient";
 
 interface registerProps {}
 
@@ -57,4 +59,4 @@ export const Login: React.FC<{}> = ({}) => {
   );
 };
 
-export default Login;
+export default withUrqlClient(createUrqlClient)(Login);
--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -6,7 +6,7 @@ import { MyContext } from "src/types";
 export class PostResolver {
   // Query all posts
   @Query(() => [Post])
-  posts(@Ctx() { em }: MyContext): Promise<Post[]> {
+  async posts(@Ctx() { em }: MyContext): Promise<Post[]> {
     return em.find(Post, {});
   }
 
